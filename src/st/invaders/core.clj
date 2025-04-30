ac504f42a4ab7ebd6167ef9ef720f529df43a5fa
Stéphane Tavera <stephane.tavera@gmail.com>
2025-04-30 15:33:48 +0200

    Add confidence in results

--- src/st/invaders/core.clj
+++ src/st/invaders/core.clj
@@ -3,35 +3,35 @@
     [st.invaders.grid :as grid]
     [st.invaders.pattern :as p]))
 
-(def ratio-signals-for-detection 0.7)
-
 (defn detect-at
-  [radar-sample i j pattern]
+  [radar-sample i j threshold-detection pattern]
   (let [pattern-grid (:grid pattern)
-        min-nb-signals-detected (* ratio-signals-for-detection (p/nb-signals pattern-grid))
-
         dim-i-pattern (grid/dim-i pattern-grid)
         dim-j-pattern (grid/dim-j pattern-grid)
-
         radar-clip (grid/clip radar-sample i j dim-i-pattern dim-j-pattern)
-        pattern-clip (grid/clip pattern-grid 0 0 (grid/dim-i radar-clip) (grid/dim-j radar-clip))]
+        pattern-clip (grid/clip pattern-grid 0 0 (grid/dim-i radar-clip) (grid/dim-j radar-clip))
 
-    (when (<= min-nb-signals-detected (p/nb-signals-matching radar-clip pattern-clip))
-      {:location [i j]
-       :id       (:id pattern)})))
+        nb-signals-pattern (p/nb-signals pattern-grid)
+        nb-matching (p/nb-signals-matching radar-clip pattern-clip)
+        confidence (/ nb-matching nb-signals-pattern)]
+
+    (when (<= threshold-detection confidence)
+        {:location             [i j]
+         :id                   (:id pattern)
+         :detection-confidence confidence})))
 
 (defn locations
-  [radar-sample invaders-patterns]
+  [radar-sample invaders-patterns threshold-detection]
   (let [dim-j (count radar-sample)
         dim-i (count (first radar-sample))
         all-coordinates (for [i (range dim-i) j (range dim-j)] [i j])]
 
     (reduce
       (fn [res [i j]]
-        (let [detections (->> (map #(detect-at radar-sample i j %) invaders-patterns)
+        (let [detections (->> (map #(detect-at radar-sample i j threshold-detection %) invaders-patterns)
                               (remove nil?))]
           (reduce (fn [acc detection]
-                    (update acc (:location detection) (fnil conj []) (:id detection)))
+                    (update acc (:location detection) (fnil conj []) detection))
                   res
                   detections)
           ))
--- test/st/invaders/core_test.clj
+++ test/st/invaders/core_test.clj
@@ -1,41 +1,82 @@
 (ns st.invaders.core-test
   (:require
-    [clojure.test :refer [deftest is]]
+    [clojure.test :refer [deftest is testing]]
     [st.invaders.core :as inv]
     [st.invaders.grid :as grid]))
 
-(def hat-pattern {:id "hat" :grid [[\- \- \-]
-                                   [\- \o \-]
+(def hat-pattern {:id "hat" :grid [[\- \o \-]
                                    [\o \- \o]]})
-
 (def line-pattern {:id "line" :grid [[\o \o \o]]})
-
 (def robot-pattern {:id "robot" :grid (grid/read-grid "robot.txt")})
-
 (def jellyfish-pattern {:id "jellyfish" :grid (grid/read-grid "jellyfish.txt")})
 
 (deftest locations-test
-  (is (= {[1 0] ["hat"]}
-         (inv/locations [[\- \- \- \-]
-                         [\- \- \o \-]
-                         [\- \o \- \o]]
-                        [hat-pattern])))
+  (testing "detection confidence"
+    (is (= {[1 1] [{:location             [1 1]
+                    :id                   "hat"
+                    :detection-confidence 1}]}
+           (inv/locations [[\- \- \- \-]
+                           [\- \- \o \-]
+                           [\- \o \- \o]]
+                          [hat-pattern]
+                          0.7)))
+    (is (= {[1 1] [{:location             [1 1]
+                    :id                   "hat"
+                    :detection-confidence 2/3}]}
+           (inv/locations [[\- \- \- \-]
+                           [\- \- \o \-]
+                           [\- \o \- \-]]
+                          [hat-pattern]
+                          0.6)))
+    (is (= {}
+           (inv/locations [[\- \- \- \-]
+                           [\- \- \o \-]
+                           [\- \o \- \-]]
+                          [hat-pattern]
+                          0.7))))
 
-  (is (= {[1 0] ["hat"]
-          [1 2] ["line"]}
+  (is (= {[1 1] [{:location             [1 1]
+                  :id                   "hat"
+                  :detection-confidence 1}]
+          [1 2] [{:location             [1 2]
+                  :id                   "line"
+                  :detection-confidence 1}]}
          (inv/locations [[\- \- \- \-]
                          [\- \- \o \-]
                          [\- \o \o \o]]
-                        [hat-pattern line-pattern])))
-  (is (= {[16 28] ["jellyfish"]
-          [35 15] ["jellyfish"]
-          [42 0]  ["jellyfish"]
-          [60 13] ["robot"]
-          [74 1]  ["robot"]
-          [77 2]  ["jellyfish"]
-          [82 41] ["jellyfish"]
-          [85 12] ["robot"]}
-         (inv/locations (grid/read-grid "radar-sample.txt")
-                        [robot-pattern jellyfish-pattern])))
+                        [hat-pattern line-pattern]
+                        0.7)))
 
+  (testing "Given example"
+    (is (= {[16 28] [{:location [16 28], :id "jellyfish", :detection-confidence 11/12}],
+            [35 15] [{:location [35 15], :id "jellyfish", :detection-confidence 8/9}],
+            [42 0]  [{:location [42 0], :id "jellyfish", :detection-confidence 31/36}],
+            [60 13] [{:location [60 13], :id "robot", :detection-confidence 19/23}],
+            [74 1]  [{:location [74 1], :id "robot", :detection-confidence 41/46}],
+            [77 2]  [{:location [77 2], :id "jellyfish", :detection-confidence 13/18}],
+            [82 41] [{:location [82 41], :id "jellyfish", :detection-confidence 8/9}],
+            [85 12] [{:location [85 12], :id "robot", :detection-confidence 20/23}]}
+           (inv/locations (grid/read-grid "radar-sample.txt")
+                          [robot-pattern jellyfish-pattern]
+                          0.7))))
   )
+
+(deftest locations-overlap-test
+  (is (= {[0 1] [{:detection-confidence 2/3
+                  :id                   "line"
+                  :location             [0 1]}]
+          [1 1] [{:detection-confidence 1
+                  :id                   "hat"
+                  :location             [1 1]}
+                 {:detection-confidence 2/3
+                  :id                   "line"
+                  :location             [1 1]}]
+          [1 2] [{:detection-confidence 2/3
+                  :id                   "line"
+                  :location             [1
+                                         2]}]}
+         (inv/locations [[\- \- \- \-]
+                         [\- \o \o \-]
+                         [\- \o \- \o]]
+                        [hat-pattern line-pattern]
+                        0.6))))
